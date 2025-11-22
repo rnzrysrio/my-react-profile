ce35bfe9e19a5561c8b0e081e8e3672f36439144
rnzrysrio <renzreysario312@gmail.com>
2025-11-22 15:15:13 +0800

    Added Image Resources

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Tech from "./components/Tech";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Tech />
       <Projects />
       <Contact />
     </div>
